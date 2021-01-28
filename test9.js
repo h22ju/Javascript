d329fec1942983f98aa4a1e809fdc9a3b915c194
h22ju <76121929+h22ju@users.noreply.github.com>
2021-01-28 16:16:23 +0900

    Add files via upload

---
+++ test9.js
@@ -0,0 +1,45 @@
+function calculate(commmand, a, b) {
+  switch (commmand) {
+    case "add":
+    case "+":
+      console.log(`${a}+${b} = ${a + b}`);
+      break;
+    case "substract":
+    case "-":
+      console.log(`${a}-${b} = ${a - b}`);
+      break;
+    case "divide":
+    case "/":
+      console.log(`${a}/${b} = ${a / b}`);
+      break;
+    case "multiply":
+    case "*":
+      console.log(`${a}*${b} = ${a * b}`);
+      break;
+    case "remainder":
+    case "%":
+      console.log(`${a}%${b} = ${a % b}`);
+      break;
+
+    default:
+      console.log(`command : add, substract, divdie, multiply, remainder`);
+      break;
+  }
+}
+
+calculate("+", 1, 2);
+calculate("add", 1, 2);
+
+calculate("-", 1, 2);
+calculate("substract", 1, 2);
+
+calculate("/", 1, 2);
+calculate("divide", 1, 2);
+
+calculate("*", 1, 2);
+calculate("multiply", 1, 2);
+
+calculate("%", 1, 2);
+calculate("remainder", 1, 2);
+
+calculate("=", 1, 2);
